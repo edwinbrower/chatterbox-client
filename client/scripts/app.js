fd3df2744bff8dc636ea6adec81dcbdfe8b9cdae
edwinbrower <edwin.brower@gmail.com>
2017-02-17 20:10:09 -0800

    Got unique clickers and added classes of username

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,8 +1,11 @@
 // YOUR CODE HERE:
 var app = {
   init: function() {
-    return true;
+    app.fetch();
+    // console.log(app.storage);
+    // return true;
   },
+  storage: [],
   send: function(message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
@@ -23,14 +26,19 @@ var app = {
     $.ajax({
       url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
       type: 'GET',
-      data: JSON.stringify(message),
+      // data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent');
+        app.storage = data.results;
+        console.log('chatterbox: Message Fetched');
+        // console.log(app.storage.room1[0]);
+        // console.log(JSON.stringify(app.storage));
+        console.log(app.storage);
+        app.sortMessages();
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
+        console.error('chatterbox: Failed to fetch message', data);
       }
     });
   },
@@ -38,12 +46,30 @@ var app = {
   clearMessages: function() {
     $('#chats').children().remove();
   },
-  renderMessage: function(message) {
-    // var chat = document.createTextNode(message);
-    // $('#chats').append(chat);
+  sortMessages: function() {
 
-    // $('#chats').html(message.text);
-    $('#chats').append('<div>' + message + '</div>');
+    for (var i = 0; i < 16; i++) {
+      var chatmessage = app.storage[i];  // will sort later
+      app.renderMessage(chatmessage);
+    }
+  },
+  renderMessage: function(message) {
+    var tempUsername = message.username;
+    var $inputUsername = $('<div class = \'' + message.username + '\'>' + message.username + '</div>');
+    // $('#chats').append('<div>' + inputUsername + '</div>');
+    // debugger;
+    $('#chats').append($inputUsername);
+    // debugger;
+    // $('\'.' + message.username + '\'').click(function() { console.log( message.username ); } );
+    $inputUsername.click(function() { console.log( message.username ); } );
+
+    // document.addEventListener('click', )
+    // console.log(document.getElementsByClassName('testt'));
+    // document.getElementsByClassName('testt').addEventListener('click', function() { console.log('hello'); } );
+    // $userchat[0].addEventListener('click', function() { console.log('hello'); } );
+    //$userchat.append( '<div>' + message.text + '</div>');
+    // console.log($userchat);
+
   },
   renderRoom: function(id) {
     $('#roomSelect').append('<div id = \"' + id + '\"></div>');
@@ -57,3 +83,4 @@ var app = {
   friendList: {}
 };
 
+app.init();
